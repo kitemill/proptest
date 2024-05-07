a253d66ccde801854550bdba256cd1f383f71951
Tallak Tveide <tallak@tveide.net>
2024-05-08 00:46:59 +0200

    First version for ai mhp test

--- proptest_logger.exs
+++ proptest_logger.exs
@@ -9,9 +9,9 @@
 
 Mix.install(
   [
-    {:modbus, "~> 0.4.0"},
     # {:cannes, "~> 0.0.5"},
     {:cannes, github: "tallakt/cannes", branch: "master"},
+    {:circuits_uart, "~> 1.4.3"},
   ],
   config: [porcelain: [driver: Porcelain.Driver.Basic]]
 )
@@ -19,8 +19,6 @@ Mix.install(
 # This agent holds the data from the probe so it can be accessed asyncronically
 defmodule ProbeAndVESCAgent do
   use Agent
-
-  @vesc_status_1 9
 
   def start_link() do
     initial_value = %{p1: 0.0, p2: 0.0, p3: 0.0, p4: 0.0, p5: 0.0, p6: 0.0, p7: 0.0, p8: 0.0, temperature: 0.0, erpm_101: 0, erpm_102: 0, erpm_103: 0, erpm_104: 0}
@@ -57,17 +55,6 @@ defmodule ProbeAndVESCAgent do
         _ ->
           state
       end
-    else
-        if <<@vesc_status_1, vesc_id>> = packet.identifier when vesc_id in [101, 102, 103, 104] do
-          case packet.data do
-            <<erpm::integer-big-32, _::binary>> ->
-              Map.put(state, String.to_atom("erpm_#{vesc_id}"), erpm)
-            _ ->
-              state
-          end
-        else
-          state
-        end
     end
   end
 end
@@ -75,7 +62,7 @@ end
 
 defmodule PropTest do
   def run() do
-    csv_header = "epoch,force_x,force_y,force_z,p1,p2,p3,p4,p5,p6,p7,p8,temperature,erpm_101,erpm_102,erpm_103,erpm_104\n"
+    csv_header = "epoch,servo1,servo2,p1,p2,p3,p4,p5,p6,p7,p8,temperature\n"
 
     #
     # CAN stuff to receive from the multi hole probe
@@ -111,38 +98,41 @@ defmodule PropTest do
 
 
     # 
-    # Modbus stuff
+    # Serial stuff
     #
+    
+    IO.inspect(Circuits.UART.enumerate)
 
-    serial_gateway_ip = {192, 168, 0, 239}
-    serial_gateway_port = 502
+    {:ok, serial_pid} = Circuits.UART.start_link()
+    Circuits.UART.open(serial_pid, "ttyUSB0", speed: 115200, active: false)
 
-    rtu_address_x = 1 # module default, 9600 baud rate
-    rtu_address_y = 2
-    rtu_address_z = 3
-    modbus_address_weight_holding_registers = 0x0000
+    polling_interval = 1000 # ms
 
-    polling_interval = 250 # ms
 
-    {:ok, master} = Modbus.Master.start_link(ip: serial_gateway_ip, port: serial_gateway_port)
+    change_setpoints_fun = fn ->
 
-    polling_fun = fn ->
-      read_modbus_regs = fn (node_address) ->
-        {:ok , regs} = Modbus.Master.exec(master, {:rhr, node_address, modbus_address_weight_holding_registers, 2})
-        regs
-      end
+      t = System.system_time(:millisecond)
 
-      regs_to_val = fn [r0, r1] ->
-        <<result::integer-big-signed-size(32)>> = <<r0::integer-big-unsigned-size(16), r1::integer-big-unsigned-size(16)>>
-        result
-      end
+
+      servo1_sp = round(1500 + 400.0 * :math.sin(t / 1000.0 / 51.0 * 3.141 * 2))
+      servo2_sp = round(1500 + 400.0 * :math.sin(t / 1000.0 / 91.0 * 3.141 * 2))
+
+      Circuits.UART.write(serial_pid,<<0xff, servo1_sp::big-16, servo2_sp::big-16>>)
+      # Circuits.UART.write(serial_pid,<<0, 0xdead::big-16, 0xbeef::big-16>>)
+      # Circuits.UART.write(serial_pid,<<0, 0x1122::big-16, 0x3344::big-16>>)
+      # Circuits.UART.write(serial_pid,<<99, 98>>)
+      IO.puts("Setting PWM values #{servo1_sp} and #{servo2_sp}")
+
+      :timer.sleep(round(polling_interval / 2))
 
       timestamp =
         DateTime.now!("Etc/UTC")
         |> DateTime.to_unix(:millisecond)
 
       pressures = ProbeAndVESCAgent.get()
-      pressure_temp_list = [
+      row = [
+        servo1_sp,
+        servo2_sp,
         pressures.p1,
         pressures.p2,
         pressures.p3,
@@ -152,19 +142,9 @@ defmodule PropTest do
         pressures.p7,
         pressures.p8,
         pressures.temperature,
-        pressures.erpm_101,
-        pressures.erpm_102,
-        pressures.erpm_103,
-        pressures.erpm_104,
       ]
 
-      tmp = 
-        [rtu_address_x, rtu_address_y, rtu_address_z]
-        |> Enum.map(read_modbus_regs)
-        |> Enum.map(regs_to_val)
-        |> Enum.concat(pressure_temp_list)
-        |> Enum.join(",")
-      "#{timestamp},#{tmp}\n"
+      "#{timestamp},#{Enum.join(row, ",")}\n"
     end
 
     timestamp = 
@@ -175,9 +155,9 @@ defmodule PropTest do
     add_csv_header_to_stream = fn enum -> Stream.concat([csv_header], enum) end
 
     Stream.interval(polling_interval)
-    |> Stream.map(fn _ -> polling_fun.() end)
+    |> Stream.map(fn _ -> change_setpoints_fun.() end)
     |> add_csv_header_to_stream.()
-    |> Stream.into(File.stream!("proptest_logger_#{timestamp}.csv"))
+    |> Stream.into(File.stream!("_ai_multi_hole_probe_logger_#{timestamp}.csv"))
     |> Stream.run()
     # this will block
 
